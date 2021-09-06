9088f95d8657c2278cda9f4fc71d5962b3d0263b
Mujle <41802968+Mujle@users.noreply.github.com>
2021-09-07 00:17:57 +0200

    Update ApiGatewayServiceApplication.java

--- src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
+++ src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
 
